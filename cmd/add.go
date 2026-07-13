cc5f8480cb9cabf1cee087eb2b4c4d015a5641f4
chigirlat <lukmmanuel@gmail.com>
2026-07-13 20:38:07 +0100

    fix: label add now takes positional args (name min max)

--- cmd/add.go
+++ cmd/add.go
@@ -10,19 +10,15 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var ports []string
-
-// addCmd represents the add command
 var addCmd = &cobra.Command{
-	Use:   "add [name] [port-range]",
+	Use:   "add <name> <min> <max>",
 	Short: "Add a new label",
-	Args:  cobra.MaximumNArgs(1),
+	Args:  cobra.ExactArgs(3),
 	Long: `Add a new label to your configuration with a name and port range.
-	
-This command allows you to add a new label to your configuration file.
-The label will be added with a name and port range.`,
+
+Example:
+  koem-cli label add backend 8000 9000`,
 	RunE: func(cmd *cobra.Command, args []string) error {
-		// fmt.Println("add called", args)
 		return addLabel(args)
 	},
 }
@@ -30,7 +26,7 @@ The label will be added with a name and port range.`,
 func addLabel(args []string) error {
 	label := &impls.Label{}
 
-	if err := label.AddLabel(args[0], ports); err != nil {
+	if err := label.AddLabel(args[0], []string{args[1], args[2]}); err != nil {
 		return err
 	}
 
@@ -43,16 +39,4 @@ func addLabel(args []string) error {
 
 func init() {
 	labelCmd.AddCommand(addCmd)
-
-	addCmd.Flags().StringSliceVarP(&ports, "ports", "p", []string{}, "Port range for the label")
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// addCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// addCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
